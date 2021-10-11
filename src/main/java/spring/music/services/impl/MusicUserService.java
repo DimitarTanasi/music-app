9a6f20317bb863b42dce835305c06d092f98d0a2
Dimitar Tanasi <mitko_107@abv.bg>
2021-10-11 21:13:45 +0300

    update security config, update User controller, refactor views

--- src/main/java/spring/music/services/impl/MusicUserService.java
+++ src/main/java/spring/music/services/impl/MusicUserService.java
@@ -38,7 +38,7 @@ public class MusicUserService implements UserDetailsService {
         List<GrantedAuthority> authorities = entity
                 .getRoles()
                 .stream()
-                .map(r->new SimpleGrantedAuthority(r.getRole().name()))
+                .map(r->new SimpleGrantedAuthority("ROLE_" + r.getRole().name()))
                 .collect(Collectors.toList());
 
         return new User(entity.getUsername(),entity.getPassword(),authorities);
--- src/main/java/spring/music/web/controllers/UserController.java
+++ src/main/java/spring/music/web/controllers/UserController.java
@@ -1,12 +1,20 @@
 package spring.music.web.controllers;
 
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import spring.music.services.impl.MusicUserService;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
+
+    private final MusicUserService musicUserService;
+
+    public UserController(MusicUserService musicUserService) {
+        this.musicUserService = musicUserService;
+    }
 
     @GetMapping("/login")
     public String login(){
@@ -15,5 +23,16 @@ public class UserController {
     @GetMapping("/register")
     public String register(){
         return "register";
+    }
+
+    @PostMapping("/login-err")
+    public ModelAndView badCredentials(
+            @ModelAttribute(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY)
+                    String username){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("bad_credentials", true);
+        modelAndView.addObject("inputUsername", username);
+        modelAndView.setViewName("/login");
+        return modelAndView;
     }
 }
